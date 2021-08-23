825d865b62053e2f8d05b6682e0b62d591dd40c7
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-08-23 17:55:50 -0300

    Feat: Cria as rotas das pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-import './App.css';
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
+import Repositories from './pages/Repositories';
+import Followers from './pages/Followers';
+import Following from './pages/Following';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Inicio</h1>
-    </div>
+    <Switch>
+      <Route component={Login} exact path="/login" />
+      <Route component={Repositories} exact path="/repositories" />
+      <Route component={Followers} exact path="/followers" />
+      <Route component={Following} exact path="/following" />
+      <Route component={Home} exact path="/" />
+      <Route component={NotFound} />
+    </Switch>
   );
 }
 
