084a2c032b08dc4bfd0ad4418147ae5aa8b2b5e4
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-08-24 19:20:07 -0300

    Feat: Renderiza componentes na home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import PictureUser from '../components/home/PictureUser';
+import Header from '../components/header/Header';
+import InfoUser from '../components/home/InfoUser';
+import DataUser from '../components/home/DataUser';
+import Bio from '../components/home/Bio';
+import Footer from '../components/home/Footer';
 
 function Home() {
   return (
-    <div>
+    <>
+      <Header />
       <PictureUser />
-    </div>
+      <InfoUser />
+      <DataUser />
+      <Bio />
+      <Footer />
+    </>
   );
 }
 
