88c516a723b22ddf7f128291311bad254ad20db4
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-08-23 23:46:39 -0300

    Feat: Renderiza o componente de estilização global

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Followers from './pages/Followers';
 import Following from './pages/Following';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
+import { GlobalStyle } from './styles/GlobalStyles';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <Route component={Following} exact path="/following" />
       <Route component={Home} exact path="/home" />
       <Route component={NotFound} />
+      <GlobalStyle />
     </Switch>
   );
 }
