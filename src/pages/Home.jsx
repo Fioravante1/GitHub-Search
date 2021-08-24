9e97fce436c310b2b6eab6b234f5dcccde3c98ea
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-08-23 23:45:09 -0300

    Feat: Renderiza avatar do usuario/teste

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import PictureUser from '../components/home/PictureUser';
 
 function Home() {
   return (
     <div>
-      <h1>Inicio</h1>
+      <PictureUser />
     </div>
   );
 }
