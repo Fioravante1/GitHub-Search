aeb2189e58666da78ad14784e9addb1e10952b9e
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-08-25 20:58:29 -0300

    fixoFeat: Cria a estilização da page seguidores

---
+++ src/components/headerseguidores/styles.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const Header = styled.header`
+  width: 100%;
+  height: 100px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  background: #1F1F1F;
+`;
+
+export const Img = styled.img`
+  width: 18px;
+  height: 18px;
+`;
+
+export const QuantityFollowers = styled.h3`
+  color: #FFFFFF;
+`;
