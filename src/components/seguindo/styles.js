849e97c5bc3ec05a78f52b546ab8c552300519ac
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-08-25 21:05:28 -0300

    fixoFeat: Estilização do componente seguindo

---
+++ src/components/seguindo/styles.js
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+
+export const ContainerFollowing = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 100px;
+  border-top: 1px solid #575757;
+`;
+
+export const ContainerInfoLogin = styled.div`
+  width: 90%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const AvatarFollowing = styled.img`
+  height: 30px;
+  width: 30px;
+  margin-left: 10px;
+  border-radius: 100px;
+  border: 3px solid #FFFFFF;
+`;
+
+export const IconArrow = styled.img`
+  width: 18px;
+  height: 18px;
+  margin-left: 5px;
+`;
+
+// export const ContainerIconArrow
+export const IconArrowRight = styled.img`
+  width: 18px;
+  height: 18px;
+  margin-right: 10px;
+`;
+
+export const NameLogin = styled.p`
+  color: #ffffff;
+  margin-left: 10px;
+`;
