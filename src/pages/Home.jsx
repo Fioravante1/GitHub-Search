ac2c323523856042b4d705b71197fbdc29cf7274
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-08-25 21:08:25 -0300

    Refactor: Correção do caminho da pasta do header

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PictureUser from '../components/home/PictureUser';
-import Header from '../components/header/Header';
+import Header from '../components/home/Header';
 import InfoUser from '../components/home/InfoUser';
 import DataUser from '../components/home/DataUser';
 import Bio from '../components/home/Bio';
