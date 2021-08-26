4675e59d0c9ca9ced985c1570bb12dc77c2fd4d7
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-08-25 21:01:21 -0300

    fixoFeat: Adiciona na home componente estilizado header

--- src/components/home/styles.js
+++ src/components/home/styles.js
@@ -16,6 +16,34 @@ export const AvatarContainner = styled.div`
   }
 `;
 
+export const HeaderContainer = styled.header`
+  width: 100%;
+  height: 150px;
+  align-items: baseline;
+  color: #ffffff;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  background: #1F1F1F;
+
+  a {
+    text-decoration: none;
+    color: #ffffff;
+    font-size: 1rem;
+  }
+
+  p {
+    font-size: 1.5rem;
+  }
+
+  .conteiner__exit {
+    width: 60px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+  }
+`;
+
 export const InfoContainer = styled.div`
   width: 500px;
   height: 210px;
