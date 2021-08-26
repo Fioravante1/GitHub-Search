10f116ad1bdf08b766ec20d3d25342876c52c5c2
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-08-25 21:04:07 -0300

    fixoFeat: Estilização do componente seguidores

---
+++ src/components/seguidores/styles.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+export const ContainerFollower = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 100px;
+  border-top: 1px solid #575757;
+`;
+
+export const AvatarFollower = styled.img`
+  height: 30px;
+  width: 30px;
+  margin-left: 10px;
+  border-radius: 100px;
+  border: 3px solid #FFFFFF;
+`;
+
+export const IconArrow = styled.img`
+  width: 18px;
+  height: 18px;
+  margin-left: 5px;
+`;
+
+export const NameLogin = styled.p`
+  color: #ffffff;
+  margin-left: 10px;
+`;
